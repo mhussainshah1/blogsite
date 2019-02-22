c8206118de978b0a776e2fa6c0c35f3d5ca96178
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-02-22 09:56:05 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/BlogsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogsiteApplication.class, args);
+    }
+
+}
